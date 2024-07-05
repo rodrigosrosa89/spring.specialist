d7bbbf15f474addbab38f7a5ead7ecca60c14496
Rodrigo Rosa <rodsiqueirabh@hotmail.com>
2024-07-04 23:43:32 -0300

    Incluindo Jakarta validation no projeto

--- src/main/java/com/rodrigo/spring/specialist/java/web/clients/dtos/ClientForm.java
+++ src/main/java/com/rodrigo/spring/specialist/java/web/clients/dtos/ClientForm.java
@@ -2,6 +2,10 @@ package com.rodrigo.spring.specialist.java.web.clients.dtos;
 
 import br.com.treinaweb.twprojects.core.utils.StringUtils;
 import com.rodrigo.spring.specialist.java.datajpa.models.Client;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,10 +17,16 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class ClientForm {
 
+    @NotBlank
+    @Size(max = 400)
     private String name;
 
+    @NotBlank
+    @Email
     private String email;
 
+    @NotBlank
+    @Pattern(regexp = "\\(\\d{2}\\) d{5}-\\d{4}$", message = "telefone deve ter formato com DDD")
     private String phone;
 
     public Client toClient() {
